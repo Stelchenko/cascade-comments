83056d302817d85574468eecbc621e5c02482272
Alexander <alexander.stelchenko@mbicycle.com>
2024-09-24 18:01:46 +0300

    prettier config gitignore config

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxSingleQuote: true,
+  singleQuote: false,
+  trailingComma: "all",
+  arrowParens: "always",
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: "auto",
+  semi: true,
+};

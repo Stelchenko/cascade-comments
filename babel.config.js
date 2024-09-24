92eb07f11eb7c92c34419834d94482dd90f7c790
Alexander <alexander.stelchenko@mbicycle.com>
2024-09-24 19:00:06 +0300

    babel module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ["module:@react-native/babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "^@screens/(.+)": "./src/screens/\\1",
+          "^@navigation/(.+)": "./src/navigation/\\1",
+          "@components": "./src/components",
+          "@types": "./src/types",
+          "@api": "./src/api",
+          "@queries": "./src/queries",
+        },
+      },
+    ],
+  ],
 };
